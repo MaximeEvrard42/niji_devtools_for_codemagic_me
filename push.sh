c4b53d9072bb9e4062494d7833330657687644d5
mevrard <maxime.evrard@niji.fr>
2021-11-19 14:55:30 +0100

    first commit

---
+++ push.sh
@@ -0,0 +1,7 @@
+echo "# niji_devtools_for_codemagic_me" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MaximeEvrard42/niji_devtools_for_codemagic_me.git
+git push -u origin main
